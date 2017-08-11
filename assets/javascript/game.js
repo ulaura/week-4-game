27abc3cea4d7439e9c2266f6dc1e1ce99b96935f
Laura Unaeze <l.unaeze5@gmail.com>
2017-08-10 21:17:57 -0700

    Started writing out JS variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+var computerChoice = Math.floor(Math.random() * 120 + 1);
+//the computer needs to randomize between 19 and 120...
+
+console.log(computerChoice); //test
+
+var crystal_1 = Math.floor(Math.random() * 12 + 1); 
+var crystal_2 = Math.floor(Math.random() * 12 + 1); 
+var crystal_3 = Math.floor(Math.random() * 12 + 1); 
+var crystal_4 = Math.floor(Math.random() * 12 + 1); 
+
+console.log(crystal_1, crystal_2, crystal_3, crystal_4); //test
+
+var userCounter = 0;
+//the number the user adds by clicking each crystal
+
+var wins = 0;
+var losses = 0;
+
+
